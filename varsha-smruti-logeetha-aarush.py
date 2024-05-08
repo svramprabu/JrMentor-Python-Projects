e85531fb01de95a534beee9150d8c4132626f60d
ramprabusv-guvi <167668962+ramprabusv-guvi@users.noreply.github.com>
2024-05-08 19:35:30 +0530

    commit 08/05/24

---
+++ varsha-smruti-logeetha-aarush.py
@@ -0,0 +1,54 @@
+# print('Hi all') # printing hi all here
+
+# Comments
+# 1. Describe your code
+# 2. skip a line of code or block of code
+#     single line
+#         -go to that line and press ctrl + /
+#     multiple line
+#         - select the code and press ctrl + /
+
+
+# print('Welcome back')
+
+# a = 10 #int
+# b = 15.5 #float
+# sum_of_num = a+b
+# print(sum_of_num) #float
+# print(type(sum_of_num))
+
+# a
+# a = int(a)
+# float(a)
+# str(a)
+
+# a = 10 #int
+# b = '15' #str
+# b = int(b) #typecasting
+# sum_of_num = a+b
+# print(sum_of_num)
+
+# print('Hiiiii')
+
+# x = input('Your name: ')
+# print(x)
+# print(type(x))
+#
+# y = int(input('y=')) # input -> convert to int -> store in y
+# print(y)
+# print(type(y))
+#
+# z = float(input('z='))
+# print(z)
+# print(type(z))
+
+# a = 123
+# b = 13.7
+# c = 'Name'
+#
+# print(a,b,c)
+# print(a,b,c,sep='->')
+#
+# print('Hiii',end='--->')
+# print('Hello')
+
